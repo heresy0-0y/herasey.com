91f1518271491fc33f9681946bad2c48af04b641
heresy0-0y <heresy.y.ysereh@gmail.com>
2021-04-05 14:22:51 -0500

    build init

---
+++ client/src/components/Proficiencies/Proficiencies.elements.js
@@ -0,0 +1,101 @@
+import styled from "styled-components";
+import {
+  SiStyledComponents,
+  SiCss3,
+  SiDigitalocean,
+  SiGithub,
+  SiGit,
+  SiJavascript,
+  SiJsonwebtokens,
+  SiReact,
+  SiReactrouter,
+  SiRss,
+  SiRuby,
+  SiRails,
+  SiNetlify,
+  SiNodemon,
+  SiNodeDotJs,
+  SiNeovim,
+  SiPostgresql,
+  SiMongodb,
+  SiPython,
+  SiHtml5,
+  SiUbuntu,
+  SiLinkedin,
+  SiLinux,
+  SiJava,
+  SiDocker,
+  SiCplusplus,
+  SiCsharp,
+  SiHeroku,
+  SiHackerrank,
+  SiSass,
+  SiElectron,
+  SiArduino,
+  SiAngular,
+  SiAdobephotoshop,
+  SiAdobexd,
+  SiAdobepremiere,
+} from "react-icons/si";
+import { Container } from "../../globalStyles";
+
+export const IconsContainer = styled(Container)`
+  border-bottom: 5px solid papayawhip;
+  display: flex;
+  justify-content: space-between;
+
+  & > * {
+      font-size: 5em;
+      padding: .4em;
+  }
+
+  ${Container}
+`;
+
+export const StyledComponentsIcon = styled(SiStyledComponents)`
+`;
+
+export const CssIcon = styled(SiCss3)`
+`;
+
+export const DigitalOceanIcon = styled(SiDigitalocean)`
+`;
+
+export const GithubIcon = styled(SiGithub)`
+`;
+
+export const GitIcon = styled(SiGit)`
+`;
+
+export const JsIcon = styled(SiJavascript)`
+`;
+
+export const JsonWebTokensIcon = styled(SiJsonwebtokens)`
+`
+
+export const ReactIcon = styled(SiReact)`
+`
+
+export const ReactRouterIcon = styled(SiReactrouter)`
+`
+
+export const RssIcon = styled(SiRss)`
+`
+
+export const RubyIcon = styled(SiRuby)`
+`
+
+export const RailsIcon = styled(SiRails)`
+`
+
+export const HerokuIcon = styled(SiHeroku)`
+`
+
+export const NodeJsIcon = styled(SiNodeDotJs)`
+`
+export const NodemonIcon = styled(SiNodemon)`
+`
+
+export const NetlifyIcon = styled(SiNetlify)``
+
+export const NeovimIcon = styled(SiNeovim)``
