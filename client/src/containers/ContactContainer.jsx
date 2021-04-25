55267c1b23c8f02d8ded8ba3f62fac1eb52e4c71
heresy0-0y <heresy.y.ysereh@gmail.com>
2021-04-24 20:30:21 -0500

    digiOcean the case of variables maybe

--- client/src/containers/ContactContainer.jsx
+++ client/src/containers/ContactContainer.jsx
@@ -2,17 +2,15 @@ import emailjs from 'emailjs-com'
 import {ContactForm} from '../components'
 
 export default function ContactContainer(props) {
+    const {SERVICE_ID, TEMPLATE_ID, USER_ID} = process.env
     const sendMessage = (e) => {
         e.preventDefault();
-        const userId = `${process.envs.userId}`
-        const serviceId = `${process.envs.serviceId}`
-        const templateId = `${process.envs.templateId}`
 
-        emailjs.sendForm(serviceId, templateId, e.target, userId)
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
-                console.log(error, userId, process.env)
+                console.log(error)
             })
     }
     return (
