18f6b20ea26183c5222eb26b977ec0e2decb6f7b
heresy0-0y <heresy.y.ysereh@gmail.com>
2021-05-02 16:53:55 -0500

    form submission confirmation

--- client/src/components/ContactForm/ContactForm.js
+++ client/src/components/ContactForm/ContactForm.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { Form, FormContainer, TextInput, MessageArea, Label, SendInput} from "./ContactForm.elements";
+import { Form, FormContainer, TextInput, Modal, MessageArea, ModalText, Label, SendInput} from "./ContactForm.elements";
 
 const ContactForm = (props) => {
-  const {sendMessage } = props
+  const {sendMessage, messageSent} = props
   return (
     <>
-    <FormContainer>
-      <Form className="contact-form" onSubmit={sendMessage}>
-          <Label>name</Label>
-          <TextInput type="text" name="from_name" required/>
-          <Label>email</Label>
-          <TextInput type="text" name="reply_to" required/>
-          <Label>message</Label>
-          <MessageArea name="message"/>
-          <SendInput type="submit" value="send"/>
-      </Form>
-      </FormContainer>
+      <Modal messageSent={messageSent}>
+       <ModalText> thanks for reaching out ! <br/> i'll get back to you as soon as i can</ModalText>
+      </Modal>
+        <FormContainer>
+          <Form className="contact-form" onSubmit={sendMessage}>
+            <Label>name</Label>
+            <TextInput type="text" name="from_name" required/>
+            <Label>email</Label>
+            <TextInput type="text" name="reply_to" required/>
+            <Label>message</Label>
+            <MessageArea name="message"/>
+            <SendInput type="submit" value="send"/>
+          </Form>
+        </FormContainer>
     </>
   );
 };
