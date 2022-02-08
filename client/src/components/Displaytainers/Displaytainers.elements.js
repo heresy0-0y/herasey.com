49e676befbf819af9805e72d4b101a5f1b1a0a9a
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-02-08 12:47:07 -0600

    carousel up and running, no links yet

--- client/src/components/Displaytainers/Displaytainers.elements.js
+++ client/src/components/Displaytainers/Displaytainers.elements.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import {Carousel, Image} from 'grommet';
 
-
-
+export const Sitesplay = styled(Carousel)`
+    max-width: 100%;
+    min-width: fit-content;
+    wrap: true;
+`;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,4 @@ export { default as Wrapper } from "./Wrapper/Wrapper.js";
 export {default as Navbar} from "./Navbar/Navbar.js"
 export {default as Proficiencies} from "./Proficiencies/Proficiencies.js"
 export {default as ContactForm} from "./ContactForm/ContactForm.js"
+export {default as Displaytainers} from './Displaytainers/Displaytainers.js'
