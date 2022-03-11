1f5fd7faffe6d42da5f33de402130d6242895ccd
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-03-11 13:46:39 -0600

    splaytainer adds'

--- client/src/components/Displaytainers/Displaytainers.js
+++ client/src/components/Displaytainers/Displaytainers.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useState, useEffect} from 'react'
-import {Sitesplay, Sitesplaytainer, Overlay} from "./Displaytainers.elements";
+import {Sitesplay, Sitesplaytainer, Overlay, Deployed, OverlayText} from "./Displaytainers.elements";
 import {Image, Button} from 'grommet';
 
 function Displaytainers() {
@@ -9,18 +9,18 @@ function Displaytainers() {
     let overlayText = 0;
 
     if (showOverlay === 1) {
-        overlayText = <p>This is the first app I ever built. Vanilla JS, HTML, and CSS. <br/> Deployed with netlify <a href="https://biospace.netlify.app/" target="_blank">here</a></p>
+        overlayText = <p>This is the first app I ever built. Vanilla JS, HTML, and CSS. <br/> Deployed with netlify <Deployed to={{ pathname: "https://biospace.netlify.app/" }}target="_blank">here</Deployed></p>
     }
     if (showOverlay === 2) {
-        overlayText = <p>This is the first app I built utilizing React.js<br/> Deployed with netlify <a href="https://mediasink.netlify.app/" target="_blank">here</a></p>
+        overlayText = <p>This is the first app I built utilizing React.js<br/> Deployed with netlify <Deployed to={{ pathname: "https://mediasink.netlify.app/" }} target="_blank">here</Deployed></p>
     }
 
     if (showOverlay === 3) {
-        overlayText = <p>This is my first MERN full stack app, and first project built as part of a team.<br/>Deployed with netlify <a href="https://faremarket.netlify.app/" target="_blank">here</a></p>
+        overlayText = <p>This is my first MERN full stack app, and first project built as part of a team.<br/>Deployed with netlify <Deployed to={{ pathname: "https://faremarket.netlify.app/" }} target="_blank">here</Deployed></p>
     }
 
     if (showOverlay === 4) {
-        overlayText = <p>My first Ruby on Rails app!<br/>Deployed with netlify <a href="https://vitafinder.netlify.app/" target="_blank">here</a></p> 
+        overlayText = <p>My first Ruby on Rails app!<br/>Deployed with netlify <Deployed to={{ pathname: "https://vitafinder.netlify.app/" }} target="_blank">here</Deployed></p> 
     }
     
 
@@ -29,7 +29,7 @@ function Displaytainers() {
         <Sitesplaytainer>
             <Sitesplay fill play="10000" wrap="true" controls="arrows">
 
-                <Image src="https://i.ibb.co/2g3ZBXN/screenshot1.png" label="show" alt="screen shot of my first vanilla js app" href="https://biospace.netlify.app/" fit="contain" onClick={()=>setShowOverlay(1)}/>
+                <Image src="https://i.ibb.co/2g3ZBXN/screenshot1.png" label="show" alt="screen shot of my first vanilla js app" fit="contain" onClick={()=>setShowOverlay(1)}/>
                 <Image src="https://i.ibb.co/jGThKp3/screen-shot-2.png" alt="screen shot of my first react app" fit="contain" onClick={()=>setShowOverlay(2)}/>
                 <Image src="https://i.ibb.co/wdstWPQ/screen-shot-3.png" alt="screen shot of my first full-stack app, using Express.js and MongoDB" fit="contain" onClick={()=>setShowOverlay(3)}/>
                 <Image src="https://i.ibb.co/YPss7Db/screen-shot-4.png" alt="screen shot of my first Ruby on Rails full-stack app" fit="contain" onClick={()=>setShowOverlay(4)}/>
