0c4128e5abc3ce8980fbe6fcd9409ca3d782684e
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-03-11 13:46:39 -0600

    splaytainer adds'

--- client/src/components/Displaytainers/Displaytainers.elements.js
+++ client/src/components/Displaytainers/Displaytainers.elements.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
-import {Carousel, Layer, Button} from 'grommet';
-import { Container} from "../../globalStyles";
+import {Link} from "react-router-dom";
+import {Carousel, Layer, Button as GButton} from 'grommet';
+import { Container, Button} from "../../globalStyles";
 
 export const Sitesplay = styled(Carousel)`
 
@@ -16,8 +17,28 @@ export const Sitesplaytainer = styled(Container)`
 `;
 
 export const Overlay = styled(Layer)`
-`
+ color: #bbdbb4;
+ text-decoration: none;
+ background-image: linear-gradient(
+    hsla(249, 65%, 35%, 1),
+    hsla(248, 52%, 39%, 1),
+    hsla(222, 39%, 42%, 1.00),
+    hsla(168, 47%, 59%, 1)
+    );
+${Container}
+`;
 
-export const OverlayButton = styled(Button)`
+export const OverlayButton = styled(GButton)`
+${Button}
+`;
 
-`
+export const OverlayText = styled.p`
+
+`;
+
+export const Deployed = styled(Link)`
+  color: #bbdbb4;
+  font-family: Montserrat;
+  text-decoration: none;
+
+`;
