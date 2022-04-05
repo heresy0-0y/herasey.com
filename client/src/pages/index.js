22b44855fe7651c89c876d922038c70121d12ec1
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-04-05 18:18:44 -0500

    nav bg fix

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,17 +1,11 @@
 import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from "@chakra-ui/react";
+  About
+} from "../components/About";
 import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
 import { Nav } from "../components/Nav";
 import { Hero } from "../components/Hero";
 import { Container } from "../components/Container";
 import { Main } from "../components/Main";
-import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import { CTA } from "../components/CTA";
 import { Footer } from "../components/Footer";
 import { Skills } from "../components/Skills";
@@ -20,12 +14,12 @@ const Index = () => (
   <Container height="100vh">
     <Hero />
     <Main>
-      <Text></Text>
+      <About />
       <Skills />
     </Main>
     <Nav />
     <Footer>
-      <Text></Text>
+
     </Footer>
   </Container>
 );
