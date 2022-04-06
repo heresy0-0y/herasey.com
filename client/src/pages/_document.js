e63647b4f0e612dd257c67e3aaf325e26614a1ac
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-04-06 13:59:23 -0500

    gitignore fix

--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -5,10 +5,7 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head>
-       
-
-        </ Head >
+        <Head/>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
