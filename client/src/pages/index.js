1d96e0485704415eeb5143354b2d45145a2a98af
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-04-06 13:20:59 -0500

    build cleanup

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import {
   About
 } from "../components/About";
@@ -11,6 +12,15 @@ import { Footer } from "../components/Footer";
 import { Skills } from "../components/Skills";
 
 const Index = () => (
+ <>
+ <Head>
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <link rel="manifest" href="/site.webmanifest" />
+  <link rel="mask-icon" href="/safari-pinned-tab.svg"  />
+  <meta name="theme-color" content="#ffffff" />
+  </Head>
   <Container height="100vh">
     <Hero />
     <Main>
@@ -22,6 +32,7 @@ const Index = () => (
 
     </Footer>
   </Container>
+  </>
 );
 
 export default Index;
