46177677e163df57d4661ebcd92d47a9580c0723
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-04-06 08:58:07 -0500

    readme

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,22 +1,21 @@
-import { Flex, Heading, VStack } from '@chakra-ui/react'
+import { Flex, Heading, VStack, Text } from '@chakra-ui/react'
 
 export const Hero = ({ title, title2 }) => (
   <Flex height="100vh">
-    <VStack mt="4rem" mr="17rem" >
-    <Heading
+    <VStack mt="4rem" mr="3rem" >
+    <Text
       fontSize="10vw"
-      bgGradient="linear(to-l, blue.600, blue.800)"
+      bgGradient="linear(to-l, teal.700, blue.800)"
       bgClip="text"
+      orientation="horizontal"
       >
-        {title}
-    </Heading>
+        hera sey
+    </Text>
     
-    <Heading
-      fontSize="10vw"
-      bgGradient="linear(to-l, blue.800, blue.600)"
-      bgClip="text"
-      >
-        {title2}
+    
+      <Heading    fontSize="10vw" bgClip="text"  bgGradient="linear(to-l, blue.800, teal.700)"
+>
+        software engineer
       </Heading>
       </VStack>
   </Flex>
