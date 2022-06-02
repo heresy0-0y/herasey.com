05cf5d4ca2282be389cbbfa81200d222931de88d
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-06-02 13:50:25 -0500

    screenshots update

--- client/src/screens/Projects/Projects.jsx
+++ client/src/screens/Projects/Projects.jsx
@@ -1,30 +1,34 @@
-import React, { useState } from "react";
-import Image from "next/image";
 import {
-  Flex,
-  Box,
   useColorMode,
-  HStack,
-  IconButton,
   useBreakpointValue,
 } from "@chakra-ui/react";
 import { Carousel } from "../../components/Carousel";
 import {
-  desktopLanding,
-  desktopSearch,
-  mobileLanding,
-  mobileSearch,
+  desktopLandingLight,
+  desktopSearchLight,
+  desktopFocusLight,
+  desktopFocusDark,
+  desktopSearchDark,
+  desktopLandingDark,
+  mobileLandingLight,
+  mobileSearchLight,
+  mobileFocusLight,
+  mobileFocusDark,
+  mobileSearchDark,
+  mobileLandingDark,
 } from "../../../public/";
 
-export default function () {
-  const mobileSlides = [
-    { src: mobileLanding, alt: "sounds alive home page screenshot" },
-    { src: mobileSearch, alt: "sounds alive search screenshot" },
-  ];
-  const desktopSlides = [
-    { src: desktopLanding, alt: "sounds alive home page screenshot" },
-    { src: desktopSearch, alt: "sounds alive search screenshot" },
-  ];
+export default function() {
+  const { colorMode } = useColorMode();
+  const mobileLanding = { light: mobileLandingLight, dark: mobileLandingDark };
+  const desktopLanding = { light: desktopLandingLight, dark: desktopLandingDark };
+  const mobileSearch = { light: mobileSearchLight, dark: mobileSearchDark };
+  const desktopSearch = { light: desktopSearchLight, dark: desktopSearchDark };
+  const mobileFocus = { light: mobileFocusLight, dark: mobileFocusDark };
+  const desktopFocus = { light: desktopFocusLight, dark: desktopFocusDark };
+
+  const mobileSlides = [{ src: mobileLanding[colorMode], alt: "sounds alive home page screenshot" }, { src: mobileSearch[colorMode], alt: "sounds alive search screenshot" }, { src: mobileFocus[colorMode], alt: "sounds alive bird focus screenshot" }];
+  const desktopSlides = [{ src: desktopLanding[colorMode], alt: "sounds alive home page screenshot" }, { src: desktopSearch[colorMode], alt: "sounds alive search screenshot" }, { src: desktopFocus[colorMode], alt: "sounds alive bird focus screenshot" }];
   const slides = useBreakpointValue({ base: mobileSlides, md: desktopSlides });
 
   return <Carousel slides={slides} />;
