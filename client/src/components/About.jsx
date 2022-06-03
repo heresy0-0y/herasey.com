bf5123d98485a229f9efbd38e46f9d0d40442e9c
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-06-03 12:40:49 -0500

    updating about me section

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,8 +1,8 @@
 import { Text, Center } from "@chakra-ui/react";
 
 const About = () => {
-  const aboutMe =
-    "i am a full stack developer with a passion for learning and building things that help people.";
+  const aboutMe = `i am a full stack developer
+    with a curiosity as vast as the web. i have always known i wanted to be a learner for life and hopefully make this world an easier place to thrive; when i discovered the magic of building software, i was flooded with the relief that comes with finding one's purpose.`
   return (
     <Center h="50vh">
       <Text align="center">{aboutMe}</Text>
